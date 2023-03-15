2b6e1cb1177c3e17586578b4f97fa54bd0e6a3af
Borja <borjamese@gmail.com>
2023-03-15 15:29:41 +0000

    Domain Name

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainNameGenerator() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domainName = `${pronoun[i]}${adj[j]}${noun[k]}.com`;
+        console.log(domainName);
+      }
+    }
+  }
+}
+
+console.log(domainNameGenerator());
